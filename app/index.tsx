c7f0068987af9c26327e7ae937e47b8df0575368
BDoXHoBeHuE <lofimusician10@gmail.com>
2026-08-31 17:25:14 +0300

    predeploy

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { useFonts } from 'expo-font';
 import Calculator from './components/Calculator';
+import { ActivityIndicator, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function CalculatorKeyboard() {
   const [fontsLoaded] = useFonts({
     'Orbitron-Regular': require('@/assets/fonts/Orbitron/Orbitron-Regular.ttf'),
     'Orbitron-Bold': require('@/assets/fonts/Orbitron/Orbitron-Bold.ttf'),
     'RobotoMono-Medium': require('@/assets/fonts/Roboto_Mono/RobotoMono-Medium.ttf'),
+    ...Ionicons.font,
   });
 
   if (!fontsLoaded) {
-    return null;
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
   }
 
   return (
